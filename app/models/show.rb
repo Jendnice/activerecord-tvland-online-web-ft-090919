0e6cd2a7f5a3d285ac9388ca790b8464f4bf534b
Jennifer Nicely <pipeline38@hotmail.com>
2019-10-21 01:40:38 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,4 +3,10 @@ class Show < ActiveRecord::Base
   has_many :characters
   has_many :actors, through: :characters 
   
+  def actors_list 
+    actors = []
+    self.collect do |show_characters|
+      show_characters.name 
+  end 
+  
 end
